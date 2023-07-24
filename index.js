79edb42490b87d14f61055d3f60af631f26cd217
Kelvin okumba <okumbakelvin@gmail.com>
2023-07-24 18:33:55 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const element=document.getElementById(main);
+const firstChild= main.querySelector('main');
+main.remove(main);
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = "Okumba is the champion";
+
+
